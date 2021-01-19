4a804cf6225be58cd25c350139e129147a6a955e
Redowan Ahmed <55311231+Redowan-Ahmed@users.noreply.github.com>
2021-01-19 22:22:11 +0600

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,22 @@ document.onkeydown = function(e) {
      return false;
   }
 }
+console.clear();
+const stopdata =
+"color: #ec0000;" +
+"font-size: 80px;" +
+"font-weight: bold;" +
+"text-shadow: 1px 1px 5px #000;" +
+"filter: dropshadow(color=rgb(249, 162, 34), offx=1, offy=1);";
+
+const stopcontent =
+"color: #fcb800;" +
+"font-size: 20px;" +
+"font-weight: bold;" +
+"text-shadow: 1px 1px 5px #000;" +
+"filter: dropshadow(color=rgb(249, 162, 34), offx=1, offy=1);";
+
+console.log("%cSTOPP!", stopdata);
+console.log(`%cThis is a browser feature intended for developers. If someone told you to copy and paste something here to get a product free of cost or ( hack ) someone's account or Hack Hawkerrz, it is a scam and you can't do anything from here.
+Thanks,
+Redowan Ahmed`, stopcontent);
